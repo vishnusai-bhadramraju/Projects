ca61309341a64fc4d7fdf25ab6849a24653a96a4
Vishnusai Bhadramraju <120679419+vishnusai-bhadramraju@users.noreply.github.com>
2024-05-27 08:22:45 -0500

    Update Question 4.sql

--- SQL files/Question 4.sql
+++ SQL files/Question 4.sql
@@ -6,7 +6,7 @@ receipts
 group by rewards_receipt_status
 having rewards_receipt_status = 'REJECTED' or rewards_receipt_status = 'FINISHED';
 
-What are the top 5 brands by receipts scanned for most recent month?
+/*What are the top 5 brands by receipts scanned for most recent month?
 How does the ranking of the top 5 brands by receipts scanned for the recent month compare to the ranking for the previous month?
 Which brand has the most spend among users who were created within the past 6 months?
-Which brand has the most transactions among users who were created within the past 6 months?*
+Which brand has the most transactions among users who were created within the past 6 months?*/
