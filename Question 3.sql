ef76a10649f81107b09666c48214a88516c53285
Vishnusai Bhadramraju <120679419+vishnusai-bhadramraju@users.noreply.github.com>
2024-05-27 02:13:48 -0500

    Delete Question 3.sql

--- Question 3.sql
+++
@@ -1,7 +0,0 @@
-/*When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?*/
-
-select rewards_receipt_status, avg(total_spent) as average
-from receipts
-group by rewards_receipt_status
-having rewards_receipt_status = 'REJECTED' or rewards_receipt_status = 'FINISHED'
-
